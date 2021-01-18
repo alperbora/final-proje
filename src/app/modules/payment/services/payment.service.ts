4f65a80654b422af182655e61116390f9b3e48af
alperbora <61551704+alperbora@users.noreply.github.com>
2021-01-18 15:29:22 +0300

    Update payment.service.ts

--- src/app/modules/payment/services/payment.service.ts
+++ src/app/modules/payment/services/payment.service.ts
@@ -8,8 +8,7 @@ export class PaymentService {
   constructor() { }
   payment(idOrder:string,amount:number):boolean
   {
-    //call back to function in fire base to call provider service for the payement
-    //receive if its ok or no for payement
+    
     return true;
 
   }
